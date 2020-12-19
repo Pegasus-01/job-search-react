93b1ed5b46d3ce899fa05bbbfe47ac54c6271a42
Rudranil Maity <48630058+Pegasus-01@users.noreply.github.com>
2020-12-19 09:28:44 +0530

    commit 1

---
+++ public/signup.php
@@ -0,0 +1,9 @@
+<?
+    $name = $_POST['name'];
+    $Gender = $_POST['Gender'];
+    $address = $_POST['address'];
+    $mobile = $_POST['mobile'];
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+?>
